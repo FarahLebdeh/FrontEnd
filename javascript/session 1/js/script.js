295596c2854b1ea158af33e95cbeccf9183ba96f
Farah Mahmoud Abu Lebdeh <64554822+FarahLebdeh@users.noreply.github.com>
2021-08-11 16:29:54 +0300

    Add files via upload

---
+++ javascript/session 1/js/script.js
@@ -0,0 +1,9 @@
+//alert("Hello Farah"); //message
+console.log("Hi"); //print in console
+document.getElementById('heading').innerHTML="hello";//when we call data in tag 
+var ages= 20;
+console.log("ages");
+console.log(ages);
+
+let age = prompt("age:");//alert with input
+console.log("ages");
